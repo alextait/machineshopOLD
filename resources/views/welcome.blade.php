6b51fef7fe8d10694353b6b2cef42e83785957c9
alextait <alextait@gmail.com>
2017-06-17 06:27:39 +0100

    First
    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<!-- saved from url=(0029)http://www.machineshop.co.uk/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Machine Shop | Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries. </title>
+        <!-- Meta -->
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Machine Shop supplies Special Effects services and Special Effects Hire for TV, Films and Commercials. Machine Shop are also specialists in Art Fabrication and Interactive Exhibition Design and Build, for Museums and Art Galleries in the UK and internationally.">
+        <meta name="keywords" content="Special Effects UK, Special Effect, SFX, TV FX, Special FX, Movie FX, Film Special Effects, Motion Picture Special Effects, Special Effects Hire, FX Rigs, Floor Effects, Pyrotechnics, Pyrotechnic Effects, Atmospheric effects, Models, Miniatures, Liquid Effects, Animatronics, Puppets, Motion Control, High Speed Special Effects, Film Costumes">
+        <link rel="shortcut icon" href="http://www.machineshop.co.uk/images/favicon.ico">
+        <link href="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/global.min.css">
+
+        <!--[if lt IE 10]>
+            <script src="/scripts/lib/html5shiv.min.js"></script>
+            <script src="/scripts/lib/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Google Analytics -->
+            <script>
+            window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+            ga('create', 'UA-4870570-1', 'auto');
+            ga('send', 'pageview');
+            </script>
+            <script async="" src="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/analytics.js.download">
+            </script>
+        <!-- End Google Analytics -->
+    </head>
+
+<body class="home menu-push">
+    <div class="wrapper">
+        
+<header class="fixed-banner">
+    <div class="container">
+        <div class="mobile-btn menu-btn" id="nav-toggle">
+            <span class="lines"></span>
+        </div>
+        <div class="logo">
+                <a href="http://www.machineshop.co.uk/">
+                    <img src="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/logo.png">
+                    <span class="logo-text">Machineshop</span>
+                </a>
+        </div>
+        <div class="navigation" id="menu">
+            
+<nav>
+    <ul>
+            <li>
+                <a href="http://www.machineshop.co.uk/special-effects/">Special Effects</a>
+            </li>
+            <li>
+                <a href="http://www.machineshop.co.uk/special-projects/">Special Projects</a>
+            </li>
+            <li>
+                <a href="http://www.machineshop.co.uk/hire/">Hire</a>
+            </li>
+            <li>
+                <a href="http://www.machineshop.co.uk/news/">News</a>
+            </li>
+            <li>
+                <a href="http://www.machineshop.co.uk/about-us/">About Us</a>
+            </li>
+            <li>
+                <a href="http://www.machineshop.co.uk/contact/">Contact</a>
+            </li>
+    </ul>
+</nav>
+
+        </div>
+    </div>
+</header>
+
+        
+<div class="banner">
+    <div class="swiper-container swiper-container-horizontal swiper-container-fade">
+        <div class="swiper-wrapper" style="transition-duration: 0ms;">
+                <div class="swiper-slide swiper-slide-active" style="background-image: url(&quot;/media/2268/screen-shot-2016-08-11-at-164319.png&quot;); width: 1903px; opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="image-overlay"></div>
+                    <div class="container">
+                        <div class="banner-text">
+                            <h2>Audi A4 Sport</h2>
+                            <p>Cooking Doughnuts &amp; Burning Rubber</p>
+                                <a href="http://www.machineshop.co.uk/special-projects/special-projects/audi-sport/" class="btn">View Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide swiper-slide-next" style="background-image: url(&quot;/media/2276/screen-shot-2016-08-11-at-175625.jpg&quot;); width: 1903px; opacity: 0; transform: translate3d(-1903px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="image-overlay"></div>
+                    <div class="container">
+                        <div class="banner-text">
+                            <h2>Little French Lobster</h2>
+                            <p>Gourmet stop frame animation</p>
+                                <a href="http://www.machineshop.co.uk/special-effects/models-miniatures/o2-lobster/" class="btn">View Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" style="background-image: url(&quot;/media/2199/untitled-1.jpg&quot;); width: 1903px; opacity: 0; transform: translate3d(-3806px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="image-overlay"></div>
+                    <div class="container">
+                        <div class="banner-text">
+                            <h2>Sky Fluid Viewing</h2>
+                            <p>Lovely liquids and amazing models</p>
+                                <a href="http://www.machineshop.co.uk/special-effects/liquid-effects/sky-fluid-viewing/" class="btn">View Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" style="background-image: url(&quot;/media/1936/6-jpeg.jpg&quot;); width: 1903px; opacity: 0; transform: translate3d(-5709px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="image-overlay"></div>
+                    <div class="container">
+                        <div class="banner-text">
+                            <h2>Schwartz Spices</h2>
+                            <p>Who said food and Pyro's dont mix?</p>
+                                <a href="http://www.machineshop.co.uk/special-effects/pyrotechnics-atmospherics/schwartz/" class="btn">View Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" style="background-image: url(&quot;/media/1934/article-2304607-191b3207000005dc-411_964x588.jpg&quot;); width: 1903px; opacity: 0; transform: translate3d(-7612px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="image-overlay"></div>
+                    <div class="container">
+                        <div class="banner-text">
+                            <h2>adidas</h2>
+                            <p>The Chelsea team are feeling blue</p>
+                                <a href="http://www.machineshop.co.uk/special-effects/liquid-effects/adidas-blue-liquid/" class="btn">View Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" style="background-image: url(&quot;/media/1003/banner-sony4k.jpg&quot;); width: 1903px; opacity: 0; transform: translate3d(-9515px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="image-overlay"></div>
+                    <div class="container">
+                        <div class="banner-text">
+                            <h2>Sony 4K TV</h2>
+                            <p>Three tonnes of petals made to erupt from a dormant volcano in Central America.</p>
+                                <a href="http://www.machineshop.co.uk/special-effects/pyrotechnics-atmospherics/sony-4k-tv-volcano-eruption/" class="btn">View Project</a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div class="swiper-pagination swiper-pagination-clickable"><span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span></div>
+    </div>
+</div>
+
+
+    </div>
+    
+<div class="footer-search">
+    <div class="container">
+        <div class="column-10">
+            <form action="http://www.machineshop.co.uk/search" method="GET">
+                <input type="text" name="q">
+                <button type="submit" class="btn-black">Search</button>
+            </form>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="container">
+        <h3>Follow Us</h3>
+        <div class="social">
+            <ul>
+                    <li><a href="https://www.facebook.com/MachineShopSpecialEffects?fref=ts" target="_blank"><span class="icon-facebook"></span></a></li>
+                                                                                                    <li><a href="https://www.youtube.com/user/machineshoppaul" target="_blank"><span class="icon-youtube3"></span></a></li>
+                            </ul>
+        </div>
+        <a href="mailto:info@machineshop.co.uk" class="email">info@machineshop.co.uk</a>
+        <div class="address">
+            <ul>
+                <li>MachineShop, 180 Acton Lane, Park Royal, London, NW10 7NH</li>
+                <li>Tel: 020 8961 5888</li>
+                <li>Fax: 020 8961 5885</li>
+            </ul>
+        </div>
+    </div>
+</footer>
+
+    <script type="text/javascript" src="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/jquery-1.11.3.min.js.download"></script>
+    <script type="text/javascript" src="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/swiper.jquery.min.js.download"></script>
+    <script type="text/javascript" src="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/jquery.validate.min.js.download"></script>
+    <script type="text/javascript" src="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/jquery.validate.unobtrusive.min.js.download"></script>
+    <script type="text/javascript" src="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/modal.min.js.download"></script>
+    <script type="text/javascript" src="./Machine Shop _ Special Effects services and Hire for TV, Films and Commercials; Interactive Exhibition Design and Build; Art Fabrication for Museums and Galleries._files/global.min.js.download"></script>
+</body></html>
